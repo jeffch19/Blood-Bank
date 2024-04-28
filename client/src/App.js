a897b0f995a3e3bca2da473c0310b95d15eaf268
jeffch19 <17jcummings@gmail.com>
2024-04-27 22:20:08 -0400

    installing packages and overriding default styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import { Button, Space} from 'antd';
+
 function App() {
   return (
     <div>
-      <h1>
-        VitalFlow - BloodBank
-      </h1>
+      <h1>VitalFlow - BloodBank</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
