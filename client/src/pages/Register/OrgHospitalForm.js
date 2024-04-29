0872a9fea584f93451c12788b121edf5c7842ebd
jeffch19 <17jcummings@gmail.com>
2024-04-29 19:07:40 -0400

    registration card for donar hospital and org

---
+++ client/src/pages/Register/OrgHospitalForm.js
@@ -0,0 +1,38 @@
+import { Form, Input } from 'antd'
+import TextArea from 'antd/es/input/TextArea'
+import React from 'react'
+
+function OrgHospitalForm({ type }) {
+  return (
+    <>
+      <Form.Item
+        label={type === 'hospital' ? 'Hospital Name' : 'Organization Name'}
+        name={type === 'hospital' ? 'hospitalName' : 'organizationName'}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item name='owner' label='Owner'>
+        <Input />
+      </Form.Item>
+      <Form.Item name='email' label='Email'>
+        <Input />
+      </Form.Item>
+      <Form.Item name='phone' label='Phone'>
+        <Input />
+      </Form.Item>
+      <Form.Item name='website' label='Website'>
+          <Input />
+        </Form.Item>
+      <Form.Item name='password' label='Password'>
+          <Input />
+          </Form.Item>
+          <Form.Item name='address' label='Address'
+          className='col-span-2'>
+          <TextArea />
+        </Form.Item>
+        
+    </>
+  )
+}
+
+export default OrgHospitalForm
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,33 +1,59 @@
 import React from 'react'
-import { Button, Form, Input, } from 'antd';
+import { Button, Form, Input, Radio } from 'antd';
 import { Link, NavLink } from "react-router-dom";
+import OrgHospitalForm from './OrgHospitalForm';
 
 
 function Register() {
-  const [type , setType] = React.useState('donar')
+  const [type, setType] = React.useState('donar')
+
+  const onFinish = (values) => {
+    console.log(values);
+  }
+
+
   return (
     <div className='flex h-screen items-center justify-center bg-primary'>
-      <Form 
-      layout = "vertical" 
-      className = "bg-white rounded shadow grid grid-cols-2 p-5 gap-5 w-1/2"
+      <Form
+        layout="vertical"
+        className="bg-white rounded shadow grid grid-cols-2 p-5 gap-5 w-1/2"
+        onFinish={onFinish}
       >
-        <h1 className='col-span-2 uppercase text-2xl'> 
-          <span className='text-primary'>Register - DONAR</span>
+        <h1 className='col-span-2 uppercase text-2xl'>
+          <span className='text-primary'>
+            {type.toUpperCase()} - REGISTRATION
+          </span>
           <hr />
         </h1>
-          <Form.Item label = "Name">
-            <Input />
-          </Form.Item>
-          <Form.Item label = "Email">
-            <Input />
-          </Form.Item>
-          <Form.Item label = "Phone">
-            <Input />
-          </Form.Item>
-          <Form.Item label = "Password">
-            <Input />
-          </Form.Item>
-        <Button type="primary" block className='col-span-2'>
+
+        <Radio.Group onChange={(e) => setType(e.target.value)} value={type}
+          className='col-span-2'>
+          <Radio value="donar">Donar</Radio>
+          <Radio value="hospital">Hospital</Radio>
+          <Radio value="organization">Organization</Radio>
+        </Radio.Group>
+
+        {type === "donar" && (
+          <>
+            {" "}
+            <Form.Item label="Name" name='name'>
+              <Input />
+            </Form.Item>
+            <Form.Item label="Email" name='email'>
+              <Input />
+            </Form.Item>
+            <Form.Item label="Phone" name='phone'>
+              <Input />
+            </Form.Item>
+            <Form.Item label="Password" name='password'>
+              <Input type='password' />
+            </Form.Item>
+          </>
+        )}
+
+        {type !== "donar" && <OrgHospitalForm type={type} />}
+
+        <Button type="primary" block className='col-span-2' htmlType='submit'>
           Register
         </Button>
         <NavLink to="/login" className="col-span-2 text-center text-gray-700 underline">
