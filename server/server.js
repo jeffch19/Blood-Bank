6ff35328e9c21c1e4660498db511e7e6d3253e9e
jeffch19 <17jcummings@gmail.com>
2024-04-30 20:55:00 -0400

    connected to mongo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
+require('dotenv').config();
+const dbConfig = require('./config/dbConfig'); 
 
 app.listen(port, () => console.log(`Node JS Server Started at ${port}`));
 
