7238875819a4aa555962571e6f4c7e22b6ca8be1
jeffch19 <17jcummings@gmail.com>
2024-05-04 12:32:32 -0400

    added header

--- client/src/components/ProtectedPage.js
+++ client/src/components/ProtectedPage.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { GetCurrentUser } from '../apicalls/users';
 import { message } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate, useNavigation } from 'react-router-dom';
 import { getLoggedInUserName } from '../utils/helpers';
 import { useDispatch, useSelector } from 'react-redux';
 import { SetCurrentUser } from '../redux/usersSlice';
 import { SetLoading } from '../redux/loadersSlice';
 
 function ProtectedPage({ children }) {
-  const {currentUser} = useSelector((state) => state.users);
+  const { currentUser } = useSelector((state) => state.users);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const getCurrentUser = async () => {
@@ -40,8 +40,31 @@ function ProtectedPage({ children }) {
   return (
     currentUser && (
       <div>
-        {<h1>Welcome {getLoggedInUserName(currentUser)}</h1>}
-        {children}
+        {/* header */}
+        <div className='flex justify-between items-center bg-primary text-white px-5 py3'>
+          <div>
+            <h1 className='text-2xl'>VitalFlow BloodBank</h1>
+            <span className='text-xs'>{currentUser.userType.toUpperCase()}</span>
+          </div>
+
+
+          <div className='flex items-center gap-1'>
+            <i class="ri-shield-user-line"></i>
+            <div className='flex flex-col'>
+              <span className='mr-5 text-md cursor-pointer'>{getLoggedInUserName(currentUser).toUpperCase()}</span>
+            </div>
+
+            <i className="ri-logout-box-line ml-5 cursor-pointer"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/login")
+              }}
+            ></i>
+          </div>
+        </div>
+
+        {/* body */}
+        <div className='p-5'>{children}</div>
       </div>
     )
   );
