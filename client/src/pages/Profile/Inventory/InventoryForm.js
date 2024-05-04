1d2fd4c0bfb067ca7ac1c1761364d48520c43d00
jeffch19 <17jcummings@gmail.com>
2024-05-04 17:29:44 -0400

    inventory form

--- client/src/pages/Profile/Inventory/InventoryForm.js
+++ client/src/pages/Profile/Inventory/InventoryForm.js
@@ -1,15 +1,90 @@
-import { Modal } from 'antd';
-import React from 'react';
+import { Form, Input, Modal, Radio, Select, message } from 'antd';
+import React, { useState } from 'react';
+import { getAntdInputValidation } from '../../../utils/helpers';
+import { useDispatch } from 'react-redux';
+import { SetLoading } from '../../../redux/loadersSlice';
+import { AddInventory } from '../../../apicalls/inventory';
 
 function InventoryForm({ open, setOpen, reloadData }) {
- return (
+  const [form] = Form.useForm();
+  const [inventoryType, setInventoryType] = useState("in");
+  const dispatch = useDispatch();
+
+  const onFinish = async (values) => {
+    try {
+      dispatch(SetLoading(true))
+      const response = await AddInventory({
+        ...values,
+        inventoryType,
+      });
+      dispatch(SetLoading(false));
+      if(response.success) {
+        message.success("Inventory Added Successfully")
+        setOpen(false)
+      } else {
+        throw new Error(response.message)
+      }
+    } catch (error) {
+      message.error(error.message)
+      dispatch(SetLoading(false))
+    }
+  }
+
+
+
+
+  return (
     <Modal
-      title="Add Inventory"
-      visible={open} // Note: The prop should be `visible` for controlling the visibility of the Modal
+      title="ADD INVENTORY"
+      open={open} // Note: The prop should be `visible` for controlling the visibility of the Modal
       onCancel={() => setOpen(false)}
       centered
-    ></Modal>
- );
+      onOk={() => {
+        form.submit();
+      }}
+    >
+      <Form layout="vertical" className='flex flex-col gap-3' form={form}
+      onFinish={onFinish}
+      >
+        <Form.Item label="Inventory Type">
+          <Radio.Group
+            value={inventoryType}
+            onChange={(e) => setInventoryType(e.target.value)}
+          >
+            <Radio value="in">In</Radio>
+            <Radio value="out">Out</Radio>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item label="Blood Group" name='bloodGroup'
+          rules={getAntdInputValidation()}
+        >
+          <select name="" id="">
+            <option value="a+">A+</option>
+            <option value="a-">A-</option>
+            <option value="b+">B+</option>
+            <option value="b-">B-</option>
+            <option value="ab+">AB+</option>
+            <option value="ab-">AB-</option>
+            <option value="o+">O+</option>
+            <option value="o-">O-</option>
+          </select>
+        </Form.Item>
+
+        <Form.Item label={
+          inventoryType === "out" ? "Hospital Email" : "Donar Email"}
+          name="email"
+          rules={getAntdInputValidation()}
+        >
+          <Input type="email" />
+        </Form.Item>
+
+        <Form.Item label="Quantity (ML)" name="quantity" rules={getAntdInputValidation()}>
+          <Input type="number" />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
 }
 
 export default InventoryForm;
