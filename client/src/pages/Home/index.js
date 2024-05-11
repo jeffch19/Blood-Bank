6528d69abc651b281e4d829b28caf620e3b352c7
jeffch19 <17jcummings@gmail.com>
2024-05-11 18:52:33 -0400

    undefined data error

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { SetLoading } from '../../redux/loadersSlice';
+import { GetAllBloodGroupsInInventory } from '../../apicalls/dashboard';
+import { message } from 'antd';
+import { getLoggedInUserName } from '../../utils/helpers';
 
 function Home() {
   const { currentUser } = useSelector((state) => state.users);
+  const [bloodGroupsData = [], setBloodGroupsData] = useState([]);
+  const dispatch = useDispatch();
+
+  const getData = async () => {
+    try {
+      dispatch(SetLoading(true));
+      const response = await GetAllBloodGroupsInInventory();
+      dispatch(SetLoading(false));
+      if(response.success) 
+        {
+          setBloodGroupsData(response.data);
+        } else {
+          throw new Error(response.message);
+        }
+      
+    } catch (error) {
+      message.error(error.message);
+      dispatch(SetLoading(false));
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div>
-      <h1>Home</h1>
-      {currentUser.email}
+      <span className='text-primary text-2xl'>
+        Welcome {getLoggedInUserName(currentUser)}
+      </span>
     </div>
   )
 }
