b0095b7faa2ddf116597701ba29f99bbd44071b7
jeffch19 <17jcummings@gmail.com>
2024-05-11 17:21:32 -0400

    postman userid error

--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ app.use(express.json());
 
 const usersRoute = require('./routes/usersRoute');
 const inventoryRoute = require('./routes/inventoryRoute');
+const dashboardRoute = require('./routes/dashboardRoute')
 
 app.use('/api/users', usersRoute);
 app.use('/api/inventory', inventoryRoute);
+app.use('/api/dashboard', dashboardRoute);
 
 app.listen(port, () => console.log(`Node JS Server Started at ${port}`));
 
