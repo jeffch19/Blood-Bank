f1f8272e4803f193cd51e9a4d32de5e729b29c64
jeffch19 <17jcummings@gmail.com>
2024-05-12 20:48:01 -0400

    adding code for deployment

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ app.use('/api/users', usersRoute);
 app.use('/api/inventory', inventoryRoute);
 app.use('/api/dashboard', dashboardRoute);
 
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Node JS Server Started at ${port}`));
 
  
